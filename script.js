916bf2315614af342a456811a60dba3c259b89d8
Filip Wawrzyniak <wawrzyniakf8@gmail.com>
2024-09-16 19:23:22 +0200

    finalna wersja strony :)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector('.nav')
 const navbtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
 
@@ -22,4 +27,22 @@ const handleNavItemsAnimation = () => {
 	})
 }
 
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerHTML = year
+}
+handleCurrentYear()
 navbtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
